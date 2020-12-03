72ae8b1704281e4572cb0fa20c99a5c2d51bd7aa
softkey <softkeydel@gmail.com>
2020-12-03 16:29:59 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'dart:async';
 import 'dart:convert';
 
@@ -75,6 +76,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   final FirebaseMessaging messaging = FirebaseMessaging();
+  final FlutterLocalNotificationsPlugin localNotifications = FlutterLocalNotificationsPlugin();
   final textEditingController = TextEditingController();
   String msg;
 
@@ -83,92 +85,113 @@ class _MyHomePageState extends State<MyHomePage> {
     // TODO: implement initState
     super.initState();
 
+    var androidSettings = new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings = new InitializationSettings(android: androidSettings, iOS: IOSInitializationSettings());
+    localNotifications.initialize(initializationSettings);
+
     messaging.configure(
-      onMessage: (message) async{
-        print(message);
-        setState(() {
-          msg = message["notification"]["body"];
-        });
-      }
+        onMessage: (message) async {
+          print(message);
+          showNotification(message['notification']['title'], message['notification']['body']);
+          setState(() {
+            msg = message["notification"]["body"];
+          });
+        }
     );
   }
 
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    textEditingController.dispose();
-    super.dispose();
+  void showNotification(String title, String body) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'channel_ID', 'channel name', 'channel description',
+        importance: Importance.max,
+        playSound: true,
+        showProgress: true,
+        priority: Priority.high);
+
+    var iOSChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics, iOS: iOSChannelSpecifics);
+    await localNotifications.show(0, title, body, platformChannelSpecifics, payload: 'test');
   }
 
+    @override
+    void dispose() {
+      // Clean up the controller when the widget is disposed.
+      textEditingController.dispose();
+      super.dispose();
+    }
 
-  void onClick() {
-    FirebaseMessaging().getToken().then((token) {
-      if(!textEditingController.text.isEmpty) {
-        sendNotification(token, textEditingController.text);
-      }
-    });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.email),
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your message',
-                ),
-                textCapitalization: TextCapitalization.sentences,
-                controller: textEditingController,
-              ),
-            ),
-            Text(
-              msg == null ? '' : 'You have a new message',
-            ),
-            Text(msg == null ? '' : '$msg',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-          ],
+    void onClick() {
+      FirebaseMessaging().getToken().then((token) {
+        if (!textEditingController.text.isEmpty) {
+          sendNotification(token, textEditingController.text);
+        }
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      // This method is rerun every time setState is called, for instance as done
+      // by the _incrementCounter method above.
+      //
+      // The Flutter framework has been optimized to make rerunning build methods
+      // fast, so that you can just rebuild anything that needs updating rather
+      // than having to individually change instances of widgets.
+      return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: onClick,
-        tooltip: 'Increment',
-        child: Icon(Icons.send),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.email),
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your message',
+                  ),
+                  textCapitalization: TextCapitalization.sentences,
+                  controller: textEditingController,
+                ),
+              ),
+              Text(
+                msg == null ? '' : 'You have a new message',
+              ),
+              Text(msg == null ? '' : '$msg',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headline5,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: onClick,
+          tooltip: 'Click',
+          child: Icon(Icons.send),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+    }
   }
-}
